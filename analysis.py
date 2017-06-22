Complete Response Time Analysis / Deadline Miss Probability computation for fixed priority.

analysis.py
@@ -5,14 +5,15 @@ probability and so forth. It makes use of the classes defined in module class_li
 -- Luca Stalder, 2017
 """
 
-from class_lib import Task, TaskSet
+import collections
 import math
-import numpy as np
-import sympy as sp
-import generation as gen
-import scipy.signal as sig
-import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as sig
+import sympy as sp
+from class_lib import TaskSet
+import generation as gen
 
 
 class BacklogSim(object):
@@ -22,26 +23,23 @@ class BacklogSim(object):
         
     Attributes:
         task_set: The TaskSet object to be analyzed. This needs priorities and release times assigned. 
-        p: The backlog's P-level, meaning only jobs of priority level P or higher are considered.
+        p_level: The backlog's P-level, meaning only jobs of priority level P or higher are considered.
         k: Number of completed hyperperiods.
         t: Elapsed time in current hyperperiod.
         backlog: The current backlog distribution, stored as a list.
-        timeline: A list of dicts, each modelling a job release with its release time and the corresponding task.
+        timeline: A list of NamedTuple, each modelling a job release with its release time and the corresponding task.
         jobs_remaining: A list of jobs that are not yet reached in the current hyperperiod.
     """
 
-    def __init__(self, task_set, p=0, initial_backlog=None):
+    def __init__(self, task_set, p_level=0, initial_backlog=None):
         self.task_set = task_set
-        self.p = p
+        self.p = p_level
         self.k = 0
         self.t = 0
         self.backlog = np.array([1.0]) if initial_backlog is None else initial_backlog  # TODO Build custom container
 
         # Build timeline of one full hyperperiod
-        self.timeline = []
-        for task in self.task_set.tasks:
-            self.timeline.extend([{'t': t, 'task': task} for t in task.rel_times if task.priority >= self.p])
-        self.timeline.sort(key=lambda x: x['t'])
+        self.timeline = [rel for rel in task_set.job_releases if rel.task.priority >= self.p]
         self.jobs_remaining = list(self.timeline)  # Store a copy
 
     def step(self, dt=None, mode='before'):
@@ -58,7 +56,7 @@ class BacklogSim(object):
             if not self.jobs_remaining:
                 dt = self.task_set.hyperperiod - self.t  # Step to end of hyperperiod if no remaining jobs
             else:
-                dt = self.jobs_remaining[0]['t'] - self.t
+                dt = self.jobs_remaining[0].t - self.t
 
         while True:
             if not self.jobs_remaining:  # No more new jobs in this hyperperiod
@@ -74,17 +72,17 @@ class BacklogSim(object):
                     self.jobs_remaining = list(self.timeline)
                     dt -= time_remaining
                     continue
-            elif dt < self.jobs_remaining[0]['t'] - self.t or dt == self.jobs_remaining[0]['t'] and mode == 'before':
+            elif dt < self.jobs_remaining[0].t - self.t or dt == self.jobs_remaining[0].t - self.t and mode == 'before':
                 # Step does not reach or convolve next job release
                 self.backlog = shrink(self.backlog, dt)
                 self.t += dt
                 return
             else:  # Step to next job release
                 next_job = self.jobs_remaining.pop(0)
-                self.backlog = shrink(self.backlog, next_job['t'] - self.t)
-                self.backlog = convolve_and_crop(self.backlog, next_job['task'].c_pdf)
-                dt -= next_job['t'] - self.t
-                self.t = next_job['t']
+                self.backlog = shrink(self.backlog, next_job.t - self.t)
+                self.backlog = convolve_pdf(self.backlog, next_job.task.c_pdf)
+                dt -= next_job.t - self.t
+                self.t = next_job.t
 
 
 def scheduling_analysis_smc(task_set: TaskSet):
@@ -216,15 +214,14 @@ def stationary_backlog_iter(task_set: TaskSet, p_level=0, max_iter=200, epsilon=
         An array representing the probability distribution over the task set's stationary backlog at the beginning of 
         its hyperperiod.
     """
-    sim = BacklogSim(task_set=task_set, p=p_level)
+    sim = BacklogSim(task_set=task_set, p_level=p_level)
     last = sim.backlog
     dist = 1.
     i = 0
     while dist > epsilon and i <= max_iter or i < 10:
         sim.step(dt=task_set.hyperperiod, mode='before')
         dist = quadratic_difference(last, sim.backlog)
-        print(i, dist)
-        # print(sim.backlog)
+        print(dist)
         last = sim.backlog
         i += 1
     if i > max_iter:
@@ -251,16 +248,13 @@ def stationary_backlog_analytic(task_set: TaskSet, p_level=0):  # TODO Incomplet
         its hyperperiod.    
     """
     w_min = 0  # Backlog after first hyperperiod assuming all jobs require minimal execution time.
-    timeline = []
-    for task in task_set.tasks:  # Build p-level timeline of one full hyperperiod
-        timeline.extend([{'t': t, 'task': task} for t in task.rel_times if task.priority >= p_level])
-    timeline.sort(key=lambda x: x['t'])
+    timeline = task_set.job_releases
     t = 0
     while timeline:
         next_job = timeline.pop(0)
-        w_min = max(w_min - (next_job['t'] - t), 0)  # 'Shrink'
-        w_min += next_job['task'].c_min()  # 'Convolve'
-        t = next_job['t']
+        w_min = max(w_min - (next_job.t - t), 0)  # 'Shrink'
+        w_min += next_job.task.c_min()  # 'Convolve'
+        t = next_job.t
     w_min = max(w_min - (task_set.hyperperiod - t), 0)
 
     r = task_set.hyperperiod + w_min - sum([task.c_min() * len(task.rel_times) for task in task_set.tasks
@@ -298,14 +292,56 @@ def stationary_backlog_analytic(task_set: TaskSet, p_level=0):  # TODO Incomplet
     print(V, V.shape)
 
 
-def dmp_analysis_FP(task_set: TaskSet):  # TODO Incomplete
-    """Deadline miss probability analysis for fixed priority scheduled task sets."""
-    backlogs = [None] * len(task_set.tasks)
+def dmp_analysis_fp(task_set: TaskSet):
+    """
+    Deadline miss probability analysis for fixed priority scheduled task sets.
+    
+    This method calculates the response time distribution for every job during one hyperperiod, assuming steady state
+    backlog. The average over all response time distributions is then taken over all jobs belonging to one task,
+    resulting in the average response time distribution of this task.
+    
+    Deadline miss probability per task is then given by 1 - sum(average response time distribution), since only response
+    times up to and including the task's deadline are kept.
+     
+    Note that the method does not return anything, and instead writes the results directly to the corresponding Task
+    objects.
+    
+    Args:
+        task_set: The task set under consideration. Note that this has to have set (fixed) task priorities.
+    """
+    sims = [None] * len(task_set.tasks)
     for task in task_set.tasks:
-        if backlogs[task.priority] is None:
-            backlogs[task.priority] = stationary_backlog_iter(task_set)
+        if sims[task.priority] is None:
+            backlog = stationary_backlog_iter(task_set, task.priority)
+            sims[task.priority] = BacklogSim(task_set, task.priority, backlog)
 
-            # Job Response Time Analysis
+    # Job Response Time Analysis
+    Job = collections.namedtuple('Job', ['rel_time', 'task'])
+    jobs = [Job(rel_time, task) for rel_time, task in task_set.job_releases]
+    job_responses = []
+    for job in jobs:
+        sim = sims[job.task.priority]
+        sim.step(job.rel_time - sim.t, mode='before')
+        part_response = convolve_pdf(sim.backlog, job.task.c_pdf)
+        preempts = [preempt for preempt in jobs
+                    if job.rel_time <= preempt.rel_time
+                    and job.task.priority < preempt.task.priority
+                    and preempt.rel_time < job.rel_time + job.task.deadline]
+        while preempts:
+            next_preempt = preempts.pop(0)
+            head, tail, _ = np.split(part_response,
+                                     [next_preempt.rel_time - job.rel_time + 1, job.task.deadline + 1])
+            tail_lim = job.task.deadline - head.size + 1
+            if tail.size:
+                tail = sig.convolve(tail, next_preempt.task.c_pdf[:tail_lim])[:tail_lim]
+            part_response = np.concatenate((head, tail))
+        part_response.resize(job.task.deadline + 1)
+        job_responses.append((job, part_response))
+
+    for task in task_set.tasks:
+        task_responses = [res for j, res in job_responses if j.task == task]
+        task.avg_response = np.sum(task_responses, axis=0) / len(task_responses)
+        task.dmp = 1. - np.sum(task.avg_response)
 
 
 def plot_backlogs(task_set, p_level=0, n_subplots = 9, add_stationary=False, scale='log'):
@@ -371,7 +407,7 @@ def animate_backlog(ts):  # TODO Fix animation (copy into separate script)
     plt.show()
 
 
-def convolve_and_crop(a, b, percentile=1-1e-14):
+def convolve_pdf(a, b, percentile=1 - 1e-14):
     """Convolution of two discrete probability distribution functions."""
     # Convolve
     conv = sig.convolve(a, b)
@@ -402,14 +438,13 @@ def shrink(pdf, t):
 def quadratic_difference(p: np.ndarray, q: np.ndarray):
     """L2-Norm of difference between arrays p and q."""
     if len(p) <= len(q):
-        if len(p) <= len(q):
-            a = np.array(p)
-            b = np.array(q)
-        else:
-            a = np.array(q)
-            b = np.array(p)
-        a.resize(len(b))
-        return np.linalg.norm(a - b, 2)
+        a = np.array(p)
+        b = np.array(q)
+    else:
+        a = np.array(q)
+        b = np.array(p)
+    a = np.concatenate((a, np.zeros(b.size - a.size)))
+    return np.linalg.norm(a - b, 2)
 
 
 def total_variation_distance(p: np.ndarray, q: np.ndarray):
@@ -424,14 +459,15 @@ def total_variation_distance(p: np.ndarray, q: np.ndarray):
     return max([abs(x - y) for x, y in zip(a, b)])
 
 if __name__ == '__main__':
-    # ts = gen.mc_fairgen_stoch(set_id=0, u_lo=0.9, mode='avg', max_tasks=10)
+    # ts = gen.mc_fairgen_stoch(set_id=0, u_lo=0.95, mode='avg', max_tasks=10)
     ts = gen.dummy_taskset()
     ts.set_priorities_rm()
     ts.set_rel_times_fp()
-    ts.draw()
-    print(stationary_backlog_iter(task_set=ts))
-    # stationary_backlog(ts, p_level=0)
+    # ts.draw()
+    # print(stationary_backlog_iter(task_set=ts))
+    # stationary_backlog_analytic(ts, p_level=0)
     # plot_backlogs(ts, add_stationary=True, scale='log')
+    dmp_analysis_fp(ts)
 
 
 """