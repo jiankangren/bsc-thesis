Add p_smc_monte_carlo. Add uunifast and corresponding eval. Fix numerical errors in randfixedsum. Fix bug in bounded_uniform. Remove resetting c_hi according to percentile in mc_fairgen_stoch(mode='max'). Add multiple evals and sanity checks.

analysis.py
@@ -5,16 +5,17 @@ probability and so forth. It makes use of the classes defined in module class_li
 -- Luca Stalder, 2017
 """
 
-import collections
 import math
 import copy
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import numpy as np
 import scipy.signal as sig
 import sympy as sp
 from class_lib import TaskSet
 import generation as gen
+import sortedcontainers as socon
+import time
+import itertools
 
 
 class BacklogSim(object):
@@ -121,7 +122,7 @@ def d_smc(task_set: TaskSet):
     return True
 
 
-def d_amc(task_set):
+def d_amc_rtb(task_set):
     """Deterministic AMC-rtb Response Time Analysis, as described in [2]."""
     epsilon = 1e-14
     for task in task_set.tasks:
@@ -197,7 +198,7 @@ def d_edf_vd(task_set: TaskSet):
     return u_1_1 + min(u_2_2, u_2_1 / (1 - u_2_2)) <= 1     # Note that this condition is sufficient for schedulability.
 
 
-def stationary_backlog_iter(task_set: TaskSet, p_level=0, max_iter=500, epsilon=1e-14):
+def stationary_backlog_iter(task_set: TaskSet, p_level=0, max_iter=1000, epsilon=1e-14):
     """Iterative calculation of the stationary backlog for a task set.
     
     This method finds the stationary backlog at the beginning of a task set's hyperperiod by repeatedly applying 
@@ -411,7 +412,7 @@ def p_smc(task_set: TaskSet, thresh_lo: float=1e-6, thresh_hi: float=1e-9) -> bo
 
 
 def p_amc_black_box(task_set: TaskSet,
-                    hi_mode_duration: int=20,
+                    hi_mode_duration: int=1,
                     thresh_lo: float=1e-6, thresh_hi: float=1e-9,
                     ) -> bool:  # TODO Issue: After LO-reset, system is assumed to restart with stationary backlog.
     """
@@ -445,6 +446,107 @@ def p_amc_black_box(task_set: TaskSet,
         p_failure_lo = 1. - np.prod([1. - job.dmp for job in task_set.jobs if job.task == task])
         # print(expected_switch_time / total_time, p_failure_lo, hi_mode_duration / total_time, p_failure_hi)
         if (expected_switch_time / total_time) * p_failure_lo + (hi_mode_duration / total_time) * p_failure_hi > thresh:
+            return False
+    return True
+
+
+def p_smc_monte_carlo(task_set, nhyperperiods=10000, thresh_lo=1e-6, thresh_hi=1e-9):
+
+    start_total = time.time()
+    elapsed = 0
+
+    class JobInstance(object):
+        def __init__(self, task, release, remaining):
+            self.task = task
+            self.release = release  # Tuple (hyperperiod of release, release time)
+            self.remaining = remaining
+            self.abs_deadline = (release[0] + (release[1] + self.task.deadline) // task_set.hyperperiod,
+                                 (release[1] + self.task.deadline) % task_set.hyperperiod)
+
+    def exec_backlog(dt):
+        """
+        
+        
+        dt will never exceed the time to the next release or the end of the hyperperiod.  
+        """
+        nonlocal t
+        while dt > 0:
+            if not backlog:
+                t += dt
+                break
+            elif backlog[0].remaining > dt:
+                t += dt
+                backlog[0].remaining -= dt
+                break
+            else:
+                done = backlog.pop(0)
+                t += done.remaining
+                dt -= done.remaining
+                if done.abs_deadline < (i, t):
+                    done.task.failed_hps.add(done.release[0])
+
+    ###
+    class CustomContainer(object):
+        def __init__(self):
+            self.items = [[] for i in range(len(task_set.tasks))]
+            self.size = 0
+
+        def __bool__(self):
+            return self.size > 0
+
+        def __iter__(self):
+            return self.as_list.__iter__()
+
+        def __getitem__(self, item):
+            return self.as_list.__getitem__(item)
+
+        def add(self, new):
+            self.items[new.task.static_prio].append(new)
+            self.size += 1
+
+        def pop(self, i):
+            for a in self.items[::-1]:
+                if a:
+                    self.size -= 1
+                    return a.pop(i)
+
+        @property
+        def as_list(self):
+            return list(itertools.chain.from_iterable(self.items[::-1]))
+    ###
+
+    # backlog = socon.SortedListWithKey([], key=lambda rel: (-rel.task.static_prio, rel.release))  # ~42%
+    backlog = CustomContainer()  # ~35%
+    for task in task_set.tasks:
+        task.exec_times = np.random.choice(a=range(len(task.c_pdf)),
+                                           size=nhyperperiods * (task_set.hyperperiod // task.period),
+                                           p=task.c_pdf)
+        task.failed_hps = set()  # All hyperperiods where an instance of this task missed its deadline
+    for i in range(nhyperperiods):
+        # print(i)
+        # Reset timer, build list of all releases in new hyperperiod
+        t = 0
+        releases = list(task_set.jobs)
+
+        while releases:
+            next_rel = releases.pop(0)
+            exec_backlog(next_rel.release - t)
+            exec_time, next_rel.task.exec_times = next_rel.task.exec_times[-1], next_rel.task.exec_times[:-1]
+            start = time.time()
+            backlog.add(JobInstance(task=next_rel.task, release=(i, next_rel.release), remaining=exec_time))
+            stop = time.time()
+            elapsed += stop - start
+        else:
+            exec_backlog(task_set.hyperperiod - t)
+    stop_total = time.time()
+    # print("Elapsed here:", elapsed)
+    # print("Elapsed total:", stop_total - start_total)
+    # print(100 * elapsed / (stop_total - start_total), "%")
+
+    for task in task_set.tasks:
+        p_fail = len(task.failed_hps) / nhyperperiods
+        thresh = thresh_hi if task.criticality == 'HI' else thresh_lo
+        if p_fail > thresh:
             return False
     return True
 
@@ -530,13 +632,17 @@ def total_variation_distance(p: np.ndarray, q: np.ndarray):
     return max([abs(x - y) for x, y in zip(a, b)])
 
 if __name__ == '__main__':
-    ts = gen.mc_fairgen_stoch(set_id=10, u_lo=0.95, m=1, mode='avg')
-    ts.set_priorities_rm()
-    ts.draw()
+    ts = gen.mc_fairgen_stoch(set_id=10, u_lo=0.5, m=1, mode='max')
+    # ts = gen.mc_fairgen_det(set_id=0, u_lo=0.95)
+    # ts.set_priorities_rm()
+    # print(ts.description)
+    # ts.draw()
     # # print(stationary_backlog_iter(task_set=ts))
     # # stationary_backlog_analytic(ts, p_level=0)
-    plot_backlogs(ts, add_stationary=True, scale='log')
-    # ts = gen.dummy_taskset()
+    # plot_backlogs(ts, add_stationary=True, scale='log')
+    ts = gen.dummy_taskset()
+    ts.set_priorities_rm()
+    p_smc_monte_carlo(ts)
     # dmp_analysis_fp(ts)
     # for task in ts.tasks:
     #     print("Task ID: %d, DMP: %f, Avg Response Time Dist: %s" % (task.task_id, task.dmp, task.avg_response))
@@ -546,6 +652,11 @@ if __name__ == '__main__':
     # print("Probability of mode switch:", p_switch)
     # for task in ts_mod.tasks:
     #     print("Task ID: %d, DMP: %f, Avg Response Time Dist: %s" % (task.task_id, task.dmp, task.avg_response))
+    # for i in range(100):
+    #     ts = gen.mc_fairgen_stoch(set_id=10, u_lo=0.95, m=1, mode='max', implicit_deadlines=True)
+    #     ts.set_priorities_rm()
+    #     print(p_smc_monte_carlo(ts))
+
 
 
 

evaluation.py
@@ -1,66 +1,158 @@
-import class_lib
+from class_lib import Task, TaskSet
 import generation as gen
 import analysis as ana
 import numpy as np
+import numpy.random as nprd
 import math
-import pickle
 import collections as col
 import matplotlib.pyplot as plt
+import multiprocessing as mp
+import functools as ft
+import time
 
-def schedulability_rates(util, nsets=100):
+utils = [0.2 + k * 0.05 for k in range(37)] #+ [1.0 + k * 0.05 for k in range(11)]
+Mode = col.namedtuple('Mode', ('name', 'color', 'linestyle', 'marker'))
+modes = [
+    Mode('d_smc', 'blue', 'dashed', 'o'),
+    Mode('d_amc', 'red', 'dashed', 's'),
+    Mode('p_smc', 'green', 'dashed', '^'),
+    Mode('p_amc_bb', 'magenta', 'dashed', '*'),
+    Mode('p_smc_monte_carlo', 'cyan', 'dashed', 'v')
+]
+nsets = 10
+
+
+def schedulability_rates(util):
     m = math.ceil(util)
     util /= m
-    results = {'d_smc': [], 'd_amc': [], 'p_smc': [], 'p_amc_bb': []}
     print(util, m)
+    results = {mode.name: [] for mode in modes}
     for i in range(nsets):
-        print(i)
         task_set = gen.mc_fairgen_stoch(i, u_lo=util, m=m, implicit_deadlines=True)
         task_set.set_priorities_rm()
-        # task_set.draw()
         results['d_smc'].append(ana.d_smc(task_set))
-        results['d_amc'].append(ana.d_amc(task_set))
+        results['d_amc'].append(ana.d_amc_rtb(task_set))
         results['p_smc'].append(ana.p_smc(task_set))
         results['p_amc_bb'].append(ana.p_amc_black_box(task_set))
-    return (sum(results['d_smc']) / nsets, sum(results['d_amc']) / nsets,
-            sum(results['p_smc']) / nsets, sum(results['p_amc_bb']) / nsets)
+        results['p_smc_monte_carlo'].append(ana.p_smc_monte_carlo(task_set))
+    return {mode.name: np.average(results[mode.name]) * 100 for mode in modes}
 
 
-# nsets = 100
-# utils = [0.95 + k * 0.05 for k in range(4)]
-# task_sets = []
-#
-#
-# for idx, util in enumerate(utils):
-#     task_sets.append([])
-#     for i in range(nsets):
-#         task_set = gen.mc_fairgen_stoch(idx*nsets + i, u_lo=util, implicit_deadlines=True)
-#         task_set.set_priorities_rm()
-#         task_sets[idx].append(task_set)
-#         # print(idx, i)
-#         pass
-# # pickle.dump(task_sets, open("task_sets", 'wb'))
-# # task_sets = pickle.load(open("task_sets", 'rb'))
-#
-# success_rates = []
-#
-# for idx, util in enumerate(utils):
-#     results = {'d_smc': [], 'd_amc': [], 'p_smc': [], 'p_amc_bb': []}
-#     for task_set in task_sets[idx]:
-#         print(idx, task_set.id)
-#         results['d_smc'].append(ana.d_smc(task_set))
-#         results['d_amc'].append(ana.d_amc(task_set))
-#         results['p_smc'].append(ana.p_smc(task_set))
-#         results['p_amc_bb'].append(ana.p_amc_black_box(task_set))
-#     success_rates.append((util, np.sum(results['d_smc']), np.sum(results['d_amc']),
-#           np.sum(results['p_smc']), np.sum(results['p_amc_bb'])))
-#     print(success_rates[idx])
+def plot_schedulability_rates():
 
-# pickle.dump(success_rates, open("success_rates", 'wb'))
+    rates = {mode.name: [] for mode in modes}
 
-utils = [0.8 + k * 0.01 for k in range(20)] + [1.0 + k * 0.05 for k in range(11)]
-rates_d_smc, rates_d_amc, rates_p_smc, rates_p_amc_bb = zip(*[schedulability_rates(util, nsets=100) for util in utils])
-ax_d_smc, ax_d_amc, ax_p_smc, ax_p_amc_bb = plt.plot(utils, rates_d_smc, 'b-o', utils, rates_d_amc, 'r-s',
-                                                     utils, rates_p_smc, 'g-^', utils, rates_p_amc_bb, 'm-*')
-plt.legend((ax_d_smc, ax_d_amc, ax_p_smc, ax_p_amc_bb), ('dSMC', 'dAMC', 'pSMC', 'pAMC-BB'), loc='center left')
-plt.axvline(1.0, color='black', linestyle='--')
-plt.show()
+    with mp.Pool() as pool:
+
+        start = time.time()
+        results = pool.map(schedulability_rates, utils)
+        stop = time.time()
+        print("Time elapsed:", stop - start)
+
+    plt.figure(figsize=(20, 10), dpi=200)
+    plt.xlabel('LO mode utilization')
+    plt.ylabel('Percentage of task sets schedulable')
+    for name, color, linestyle, marker in modes:
+        rates[name] = [result[name] for result in results]
+        plt.plot(utils, rates[name], label=name, color=color, linestyle=linestyle, marker=marker)
+
+    plt.legend(loc='center left')
+    plt.axhline(0.0, color='black', linestyle='--')
+    plt.axhline(100.0, color='black', linestyle='--')
+    plt.axvline(1.0, color='black', linestyle='--')
+    # plt.savefig('out.png')
+    plt.show()
+
+
+def plot_gen_util_error():
+    """Sanity check: Plots error of desired vs. generated LO task util of mc_fairgen_stoch."""
+    gran = 100  # Time granularity
+    max_errs, avg_errs, min_errs = [], [], []
+    pool = mp.Pool()
+    for util in utils:
+        print(util)
+        m = math.ceil(util)
+        # util_adj = util / m
+        util_adj = (util / m) + (1. / gran)
+        task_sets = pool.map(ft.partial(gen.mc_fairgen_stoch, u_lo=util_adj, m=m, time_granularity=gran), range(nsets))
+        errs = [task_set.u_lo - util for task_set in task_sets]
+        max_errs.append(max(errs))
+        avg_errs.append(np.average(errs))
+        min_errs.append(min(errs))
+    plt.figure(figsize=(20, 10), dpi=200)
+    plt.title("Deviation of generated LO-mode utilizations vs. input generation parameter u_lo. Time granularity:%d"
+              % gran)
+    plt.xlabel('Input parameter u_lo')
+    plt.ylabel('Desired u_lo minus actual LO-mode utilization')
+    plt.plot(utils, max_errs, 'r-v', label='Max')
+    plt.plot(utils, avg_errs, 'g-o', label='Avg')
+    plt.plot(utils, min_errs, 'b-^', label='Min')
+    plt.axhline(1./gran, color='black', linestyle='dotted')
+    plt.axhline(0., color='black', linestyle='dashed')
+    plt.axhline(-1. / gran, color='black', linestyle='dotted')
+    plt.legend(loc='upper left')
+    # plt.savefig('u_lo_deviation_adj.png')
+    plt.show()
+
+
+def plot_rta_for_mc_systems():
+    """Sanity check: Reproduce (part of) the plot seen in [1], p. 8."""
+
+    utils = [0.025 + k * 0.05 for k in range(20)]
+    modes = [
+        Mode('SMC', 'green', 'solid', 'o'),
+        Mode('AMC-rtb', 'blue', 'solid', '^'),
+    ]
+    ntasks = 20
+    nsets = 1000
+    cf = 2.0  # Fixed multiplier for high criticality execution time
+    cp = 0.5  # Probability for a task being HI-critical
+
+    rates = {mode.name: [] for mode in modes}
+    pool = mp.Pool()
+
+    for util in utils:
+        print(util)
+        results = {mode.name: [] for mode in modes}
+        task_sets = []
+        for i in range(nsets):
+            periods = 10 ** np.random.uniform(1, 3, ntasks)  # log-uniform distribution w/ factor 100 difference
+            criticalities = np.random.choice(a=['HI', 'LO'], size=ntasks, p=[cp, 1 - cp])
+            utils_lo = gen.uunifast(ntasks, util)
+            tasks = [Task(j, criticalities[j], periods[j], periods[j],
+                          utils_lo[j] * periods[j],
+                          utils_lo[j] * cf * periods[j] if criticalities[j] == 'HI' else None
+                          ) for j in range(ntasks)]
+            task_set = TaskSet(i, tasks, discrete=False)
+            task_set.set_priorities_rm()
+            task_sets.append(task_set)
+        results['SMC'] = pool.map(ana.d_smc, task_sets)
+        results['AMC-rtb'] = pool.map(ana.d_amc_rtb, task_sets)
+        for mode in modes:
+            rates[mode.name].append(np.average(results[mode.name]) * 100)
+
+    plt.figure(figsize=(20, 10), dpi=200)
+    plt.title("Deterministic schedulability, task sets generated by UUniFast.")
+    plt.xlabel('LO mode utilization')
+    plt.ylabel('Percentage of task sets schedulable')
+    for name, color, linestyle, marker in modes:
+        # rates[name] = [result[name] for result in results]
+        plt.plot(utils, rates[name], label=name, color=color, linestyle=linestyle, marker=marker)
+
+    plt.legend(loc='center left')
+    plt.axhline(0.0, color='black', linestyle='--')
+    plt.axhline(100.0, color='black', linestyle='--')
+    plt.axvline(1.0, color='black', linestyle='--')
+    plt.show()
+
+if __name__ == '__main__':
+    plot_schedulability_rates()
+    # plot_gen_util_error()
+    # plot_rta_for_mc_systems()
+
+
+"""
+[1] Baruah, Burns, Davis
+    Response-Time Analysis for Mixed Criticality Systems
+"""
+

generation.py
@@ -10,6 +10,17 @@ import numpy as np
 import numpy.random as nprd
 from class_lib import Task, TaskSet, WeibullDist
 
+def uunifast(n, util):
+    """Returns an array of n values in (0, 1) which sum up to util. See [3]."""
+    sum_u = util
+    vect_u = np.empty(n)
+    for i in range(n - 1):
+        next_sum_u = sum_u * nprd.random() ** (1.0 / (float(n - i)))
+        vect_u[i] = sum_u - next_sum_u
+        sum_u = next_sum_u
+    vect_u[-1] = sum_u
+    return vect_u
+
 
 def randfixedsum(n, u, nsets, a, b):
     """Randomly and uniformly generates vectors with a specified sum and values in a specified interval.
@@ -58,8 +69,8 @@ def randfixedsum(n, u, nsets, a, b):
     t = np.zeros((n - 1, n))
 
     for i in np.arange(2, (n+1)):
-        tmp1 = w[i - 2, np.arange(1, (i + 1))] * s1[np.arange(0, i)] / float(i)
-        tmp2 = w[i - 2, np.arange(0, i)] * s2[np.arange((n - i), n)] / float(i)
+        tmp1 = (w[i - 2, np.arange(1, (i + 1))] / float(i)) * s1[np.arange(0, i)]
+        tmp2 = (w[i - 2, np.arange(0, i)] / float(i)) * s2[np.arange((n - i), n)]
         w[i - 1, np.arange(1, (i + 1))] = tmp1 + tmp2
         tmp3 = w[i - 1, np.arange(1, (i + 1))] + tiny
         tmp4 = np.array((s2[np.arange((n - i), n)] > s1[np.arange(0, i)]))
@@ -116,11 +127,13 @@ def bounded_uniform(u_hi_lo, u_hi_hi, m, n_hi, u_min, u_hi):
         A list u_lo of task utilizations for every HI-criticality task.
     """
     u_lo = []
-    arr = sorted(u_hi, reverse=True)
+    # arr = sorted(u_hi, reverse=True)
+    u_hi[::-1].sort()
+
     u_lo_rem = u_hi_lo * m
     u_hi_rem = u_hi_hi * m
     n_hi_rem = n_hi - 1
-    for u in arr:
+    for u in u_hi:
         u_hi_rem -= u
         curr = nprd.uniform(max(u_min, u_lo_rem - u_hi_rem), min((u_lo_rem - (n_hi_rem * u_min)), u))
         n_hi_rem -= 1
@@ -168,26 +181,27 @@ def mc_fairgen_det(
     """
 
     if u_hi is None:
-        u_hi_hi = nprd.uniform(low=max_tasks*u_min, high=1.0)
+        u_hi_hi = nprd.uniform(low=max_tasks * u_min, high=1.0)
     else:
         u_hi_hi = u_hi
 
-    p_hi = nprd.randint(1, 10) / 10.0  # percentage of hi-criticality tasks
+    p_hi = nprd.randint(1, 10) / 10.  # fraction of hi-criticality tasks
     p_lo = 1. - p_hi
 
     if u_lo is None:
-        u_hi_lo = nprd.uniform(low=max_tasks*p_hi*u_min, high=u_hi_hi)
-        u_lo_lo = nprd.uniform(low=max_tasks*p_lo*u_min, high=1 - u_hi_lo)
+        u_hi_lo = nprd.uniform(low=max_tasks * p_hi * u_min, high=u_hi_hi)
+        u_lo_lo = nprd.uniform(low=max_tasks * p_lo * u_min, high=1 - u_hi_lo)
     else:
-        u_hi_lo = nprd.uniform(low=max_tasks*p_hi*u_min, high=min(u_hi_hi, u_lo) - max_tasks*p_lo*u_min)
+        u_hi_lo = nprd.uniform(low=max_tasks * p_hi * u_min, high=min(u_hi_hi, u_lo) - max_tasks * p_lo * u_min)
         u_lo_lo = u_lo - u_hi_lo
 
     n_min_hi = int(math.ceil(u_hi_hi * m / u_max))  # minimum required total tasks
     n_min_lo = int(math.ceil(u_lo_lo * m / u_max))
     n_min = max(m + 1, int(math.ceil(n_min_hi / p_hi)), int(math.ceil(n_min_lo / (1 - p_hi))))
-    if n_min > max_tasks:
-        n_min = max_tasks
-    n = nprd.randint(n_min, max_tasks * m + 1)  # total numbers of tasks
+    if n_min < max_tasks * m:  # randint only working if low < high
+        n = nprd.randint(n_min, max_tasks * m + 1)
+    else:
+        n = max_tasks * m
     n_hi = max(int(p_hi * n), n_min_hi)
     n_lo = n - n_hi
 
@@ -296,10 +310,7 @@ def mc_fairgen_stoch(
                 t.c_lo = math.ceil(distribution.percentile(p=c_lo_percent))
                 if t.c_lo > t.deadline:
                     break
-            if t.criticality == 'HI':
-                t.c_hi = math.ceil(distribution.percentile(p=c_hi_percent))
-                if t.c_hi > t.deadline:
-                    break
+
             cutoff = c_hi_percent if t.criticality == 'HI' else c_lo_percent
             t.c_pdf = distribution.discrete_pd(cutoff=cutoff)
         else:
@@ -329,4 +340,6 @@ Literature:
     Techniques For The Synthesis Of Multiprocessor Tasksets
 [2] Ramanathan, Easwaran
     Evaluation of Mixed-Criticality Scheduling Algorithms using a Fair Taskset Generator
+[3] Bini, Buttazzo
+    Measuring the Performance of Schedulability Tests
 """